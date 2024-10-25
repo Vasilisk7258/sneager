dc021ed50f1f00cc771556e4841bf0eee18762f9
vasilisk7258 <ertybasevk@gmail.com>
2024-10-25 12:58:45 +0300

    0.0.3

---
+++ src/components/CardList.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Card from './Card.vue'
+const onClickAdd = () => {
+  alert(111)
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      title="Мужские кроссовки Nike Blazer Mid Suede"
+      image-url="/sneakers/sneakers-1.jpg"
+      :price="5000"
+      v-on:click-add="onClickAdd"
+    />
+    <!-- <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card /> -->
+  </div>
+</template>
